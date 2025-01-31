a9bad48cf8099c502311472653aac705a971e42a
XXS1337 <viorelshtefan@gmail.com>
2025-01-31 18:27:48 +0200

    modified firebase

--- firebase.ts
+++ firebase.ts
@@ -8,13 +8,13 @@ import { getFirestore, Firestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
-  measurementId: '',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: 'projectrenteasejs',
+  storageBucket: 'projectrenteasejs.firebasestorage.app',
+  messagingSenderId: '103578940217',
+  appId: '1:103578940217:web:26f5ad0835d6e8b141eee8',
+  measurementId: 'G-CZPHL65F88',
 };
 
 // Initialize Firebase
